a04c7c72d2b9b8e588a008627fdce89e489c383e
Morgan Baker <102342517+bakermor@users.noreply.github.com>
2025-08-17 12:55:26 -0500

    Update CORs origins

--- src/pixelboost/main.py
+++ src/pixelboost/main.py
@@ -28,7 +28,7 @@ api = FastAPI(
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
